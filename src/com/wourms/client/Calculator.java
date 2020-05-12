465710e5e38b513c47c5d7b7339848ab91ed7e34
Wourms <jennifer.wourms@accenture.com>
2020-05-12 12:26:09 -0700

    A simple container containing a buttongroup containing the number butttons of the calculator

--- src/com/wourms/client/Calculator.java
+++ src/com/wourms/client/Calculator.java
@@ -1,6 +1,11 @@
 package com.wourms.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.sencha.gxt.widget.core.client.ContentPanel;
+import com.sencha.gxt.widget.core.client.button.ButtonGroup;
+import com.sencha.gxt.widget.core.client.button.TextButton;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -15,7 +20,40 @@ public class Calculator implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		// TODO Auto-generated method stub
+		ContentPanel panel = new ContentPanel();
 		
+		panel.getButtonBar().add(numberPad());
+		
+		RootPanel.get().add(panel);
+	}
+
+	private ButtonGroup numberPad() {
+		ButtonGroup group = new ButtonGroup();
+		group.setHeading("ButtonGroup");
+		//panel.getButtonBar().add(group);
+		
+		FlexTable table = new FlexTable();
+		group.add(table);
+		
+		TextButton btn = new TextButton("1");
+		table.setWidget(0, 0, btn);
+		btn = new TextButton("2");
+		table.setWidget(0, 1, btn);
+		btn = new TextButton("3");
+		table.setWidget(0, 2, btn);
+		btn = new TextButton("4");
+		table.setWidget(1, 0, btn);
+		btn = new TextButton("5");
+		table.setWidget(1, 1, btn);
+		btn = new TextButton("6");
+		table.setWidget(1, 2, btn);
+		btn = new TextButton("7");
+		table.setWidget(2, 0, btn);
+		btn = new TextButton("8");
+		table.setWidget(2, 1, btn);
+		btn = new TextButton("9");
+		table.setWidget(2, 2, btn);
+		
+		return group;
 	}
 }
